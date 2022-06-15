2ce97c9076e09dd70ce07633656b85d804c949cc
Satomi N <40054261+nyancat3@users.noreply.github.com>
2022-06-16 08:52:19 +0900

    Solve problem 20 in a faster way

--- 20.valid-parentheses.py
+++ 20.valid-parentheses.py
@@ -7,9 +7,22 @@
 # @lc code=start
 class Solution:
     def isValid(self, s: str):
-        # Time complexity: O(n) ?
-        while "()" in s or "{}" in s or "[]" in s:
-            s = s.replace("()", "")
+        # Better solution: average time complexity is O(n), worst one is O(n^2)
+        if len(s) % 2 == 1:
+            return False
+        dict = {")": "(", "}": "{", "]": "["}
+        stack = []
+        for s_each in s:    # O(n)
+            if s_each in dict: # avg: O(1), worst: O(n)
+                if len(stack) == 0 or stack.pop() != dict[s_each]:
+                    return False
+            else:
+                stack.append(s_each)
+        return len(stack) == 0
+
+        # Time complexity: O(6n) ? ~= O(n)
+        while "()" in s or "{}" in s or "[]" in s: # O(n) for each in operation?
+            s = s.replace("()", "") # O(n) for each replacing operation
             s = s.replace("{}", "")
             s = s.replace("[]", "")
         if len(s) > 0:
