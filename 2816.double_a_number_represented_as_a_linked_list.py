c7d6e7a575eb5b1552b41fa6c48914df6d9a449a
Satomi N <40054261+nyancat3@users.noreply.github.com>
2023-08-14 17:38:47 +0900

    Solve 2816 with a single loop

--- 2816.double_a_number_represented_as_a_linked_list.py
+++ 2816.double_a_number_represented_as_a_linked_list.py
@@ -3,27 +3,14 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-import sys
-
 class Solution:
     def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        sys.set_int_max_str_digits(10**5)   # Avoid ValueError: Exceeds the limit (4300) for integer string conversion; use sys.set_int_max_str_digits() to increase the limit
-
-        num_list = []
-        pointer = head
-        while pointer:
-            num_list.append(pointer.val)
-            pointer = pointer.next
-
-        num = 0
-        for i in range(len(num_list)):
-            num += num_list[len(num_list) - 1 - i] * pow(10, i)
-        doubled_num = num * 2
-
-        answer_head = ListNode()
-        answer_tail = answer_head
-
-        for c in str(doubled_num):
-            answer_tail.next = ListNode(int(c))
-            answer_tail = answer_tail.next
-        return answer_head.next
+        if head.val >= 5:
+            head = ListNode(0, head)
+        node = head
+        while node:
+            node.val = node.val * 2 % 10
+            if node.next and node.next.val >= 5:    # if the value is greater than or equal to 5, carry 1
+                node.val += 1
+            node = node.next
+        return head
