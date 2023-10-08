65e260c97e82dd1bbe900d90b355b04f88c07e92
Satomi N <40054261+nyancat3@users.noreply.github.com>
2023-10-08 18:17:10 +0900

    Solve 424 in O(n) time

--- 424.longest-repeating-character-replacement.py
+++ 424.longest-repeating-character-replacement.py
@@ -9,17 +9,19 @@ class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         ans, l = 0, 0
         dict = {}   # { alphabet: count, ... } max length is 26
+        max_count = 0
+        # O(n)
         for r in range(len(s)):
             dict[s[r]] = dict.get(s[r], 0) + 1
-            max_count = max(dict.values())  # O(26)
+            max_count = max(max_count, dict[s[r]])
             current_len = r - l + 1
 
-            while current_len - max_count > k:
+            if current_len - max_count > k:
                 dict[s[l]] -= 1
                 l += 1
-                max_count = max(dict.values())
-                current_len = r - l + 1
-            ans = max(ans, current_len)
+                # no need to decrease max_count as the size of the window will be the same as long as both l and r are increasing at the same time
+            else:
+                ans = max(ans, current_len)
 
         return ans
 # @lc code=end
