5d317c1e0cc9531f7d381c4fb1f04be69c051424
Satomi N <40054261+nyancat3@users.noreply.github.com>
2023-11-20 19:06:17 +0900

    Make 2937 shorter

--- weekly_contest372/2937.make_three_strings_equal.py
+++ weekly_contest372/2937.make_three_strings_equal.py
@@ -2,17 +2,7 @@ class Solution:
     def findMinimumOperations(self, s1: str, s2: str, s3: str) -> int:
         l_s1, l_s2, l_s3 = len(s1), len(s2), len(s3)
         min_l = min(l_s1, l_s2, l_s3)
-
-        if s1[0] != s2[0] or s2[0] != s3[0]:
-            return -1
-
-        ans  = l_s1 + l_s2 + l_s3 - min_l * 3
-        same_count = 0
-        for i in range(min_l):
-            if s1[i] != s2[i] or s2[i] != s3[i]:
-                break
-            same_count += 1
-
-        ans += min_l * 3 - same_count * 3
-
-        return ans
+        i = 0
+        while i < min_l and s1[i] == s2[i] and s2[i] == s3[i]:
+            i += 1
+        return -1 if i == 0 else l_s1 + l_s2 + l_s3 - i * 3 # i * 3 is the number of common characters in the beginning of s1, s2, s3
