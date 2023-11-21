08d31afd71edd9eef8a05e024b242f5777899cca
Satomi N <40054261+nyancat3@users.noreply.github.com>
2023-11-21 23:13:13 +0900

    Add 146 and 287 template files from the linked list section

---
+++ 146.lru-cache.py
@@ -0,0 +1,39 @@
+#
+# @lc app=leetcode id=146 lang=python3
+#
+# [146] LRU Cache
+#
+
+# @lc code=start
+class Node:
+    def __init__(self, key, val) -> None:
+        self.key, self.val = key, val
+        self.prev = self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {} # map key to node
+
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            return self.cache[key]
+        return -1
+
+
+    def put(self, key: int, value: int) -> None:
+        if len(self.cache) >= self.capacity:
+            # pop
+        self.cache[key] = value
+
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+# @lc code=end
