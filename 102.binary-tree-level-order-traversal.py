e95c40c1018606a0ba5b23d6d3f1b4d9f0c9956c
Satomi N <40054261+nyancat3@users.noreply.github.com>
2023-12-29 21:08:25 +0900

    Make 102 shorter

--- 102.binary-tree-level-order-traversal.py
+++ 102.binary-tree-level-order-traversal.py
@@ -10,20 +10,15 @@ class Solution:
         if not root:
             return []
         node_que = collections.deque([root])
-        ans = [[root.val]]
+        ans = []
 
         while node_que:
-            same_height_nodes = []
+            ans.append([])
             for i in range(len(node_que)): # Cannot use node_que because left/right values will be appended in the loop
                 node = node_que.pop() # Pop from right
+                ans[-1].append(node.val)
                 if node.left:
                     node_que.appendleft(node.left) # Append from left
-                    same_height_nodes.append(node.left.val)
                 if node.right:
                     node_que.appendleft(node.right) # Append from left
-                    same_height_nodes.append(node.right.val)
-
-            if same_height_nodes:
-                ans.append(same_height_nodes)
-
         return ans
