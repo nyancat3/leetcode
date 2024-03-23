fbd21b22c13c3b55bcb3cfb0e4dc3d78a3ef613c
Satomi N <40054261+nyancat3@users.noreply.github.com>
2024-03-23 22:11:47 +0900

    Solve 287

--- 287.find-the-duplicate-number.py
+++ 287.find-the-duplicate-number.py
@@ -7,6 +7,16 @@
 # @lc code=start
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break # intersection
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow # find the entry point of the loop. 2 slow = fast
 # @lc code=end
-
